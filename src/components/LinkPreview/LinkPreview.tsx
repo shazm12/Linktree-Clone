198c72dedd8d7aafb5fc3f0c82b51d50f25bde7e
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-09-03 00:58:34 +0530

    chore: do only vite build for now;

--- src/components/LinkPreview/LinkPreview.tsx
+++ src/components/LinkPreview/LinkPreview.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const LinkPreview = ({title, description, imageURL}) => {
 
+
     const handleLinkClick = () => {
         window.open(imageURL, "_blank", "noopener,noreferrer");
     }
+
+
+    
+
 
   return (
     <div className="flex flex-col bg-green-100 py-4 px-4 justify-center items-center w-11/12 my-5 cursor-pointer rounded-xl shadow-xl border border-slate-400" onClick={handleLinkClick}>
